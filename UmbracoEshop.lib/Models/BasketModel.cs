9683b98cb5b85b5ecc44464a8eda5cb3585f0148
Jolana1 <ksenicv@icloud.com>
2022-12-21 18:40:33 +0100

    Umbraco Eshop 11-09-Pridanie produktov -Nákupný košík - časť III

--- UmbracoEshop.lib/Models/BasketModel.cs
+++ UmbracoEshop.lib/Models/BasketModel.cs
@@ -9,7 +9,27 @@ namespace UmbracoEshop.lib.Models
 {
     public class BasketModel
     {
-       public Quote QuoteData { get; set; }
+       public QuoteModel Quote { get; set; }
+        
+        public List<Product2QuoteModel> QuoteItems { get; set; }
+        public BasketModel() 
+        {
+        }
+        public BasketModel(string sessionId)                  /*Vytvorime si este aj druhy konstruktor BasketModel do ktoreho posleme sessionId a na zakladeneho sa moze sessionId automaticky nacita zoznam položiek z obj. Quote a QuoteItems*/
+        {
+            QuoteRepository quoteRep = new QuoteRepository();
+            Quote quote = quoteRep.GetForSession(sessionId);
+            this.Quote = QuoteModel.CreateCopyFrom(quote);     /*Vytvorime si tu Quotemodel*/
+
+            this.QuoteItems = new List<Product2QuoteModel>();                 /*tu si ukladame obsah položiek košika*/
+            Product2QuoteRepository p2qRep = new Product2QuoteRepository();
+            foreach (Product2Quote item in p2qRep.GetQuoteItems(quote.pk))   /*tato metoda nacita vsetky položky pre danu objednavku*/
+            {
+                this.QuoteItems.Add(Product2QuoteModel.CreateCopyFrom(item));
+
+            }
+
+
         
         public List<Product2Quote> QuoteItems { get; set; }
 
